660a0d4e0a6d455957f2c9ccf28d9646735502d4
gabrielseron <gabriel.seron.pro@gmail.com>
2021-08-01 21:52:42 +0200

    Create home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalController, Platform } from '@ionic/angular';
+import { ProfilComponent } from '../../modals/profil/profil.component';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit
+{
 
-  constructor() {}
+  constructor
+  (
+    private modal: ModalController,
+    private storage: NativeStorage,
+    private platform: Platform,
+    private router: Router,
+  ) {}
 
+  ngOnInit() 
+  {
+    
+  }
+
+    
+
+  async openProfil()
+  {
+    const modal = await this.modal.create(
+    {
+      component: ProfilComponent
+    });
+    modal.onDidDismiss().then(()=>
+    {
+      if (this.platform.is("desktop") && (!localStorage.getItem('token') || !localStorage.getItem('mailUser') || !localStorage.getItem('nameUser')))
+      {
+        this.router.navigate(['login']);
+      } else if(!this.storage.getItem('token') || !this.storage.getItem('mailUser') || !this.storage.getItem('nameUser'))
+      {
+        this.router.navigate(['login']);
+      }
+    })
+    return await modal.present();
+  }
 }
